f3976eb609a75590619a8e4dbf3774820bf86c79
John Easter <john@owltestprep.com>
2020-07-08 02:43:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(array_1 = [1, 2, 3,], array_2 = [4, 5, 6])
   array_1.concat(array_2)
 end
 
-def using_insert(array = [1, 2, 3], element = 123)
+def using_insert(array = [1, 2, 3, 4], element = 1234)
   array.insert(element)
 end
