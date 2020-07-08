085eaa2e5bf23782608d1ae6a5fdf0751e0d2f12
John Easter <john@owltestprep.com>
2020-07-08 02:48:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,11 @@ end
 def using_insert(array = [1, 2, 3, 4], element = 1234)
   array.insert(4, element)
 end
+
+def using_uniq(array = [1, 1, 2, 2, 3, 3])
+  array.uniq
+end
+
+def using_flatten(array = [1, [1], 2, [1, 4], 3, [1, 4, 9]])
+  array.flatten
+end
